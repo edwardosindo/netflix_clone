75991454bbc723197d76da768809160ccf19e795
Edward Osindo <eddu.almasi@gmail.com>
2020-12-07 12:56:08 +0300

    Final Netflix App

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect } from 'react';
 import axios from './axios';
 import "./Row.css";
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     // A snippet of code that runs based on a specific condition
     useEffect(() => {
@@ -19,7 +22,30 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchdata();
     }, [fetchUrl]);
 
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || "" )
+            .then( url => {
+                // https://www.youtube.com/watch?v=ytettetgdshjsj
+                const urlParams = new URLSearchParams (new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+
+            })
+            .catch(error => console.log(error));
+        }
+    }
     //console.log(movies);
+
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
 
     return (
         <div className="row">
@@ -30,14 +56,14 @@ function Row({ title, fetchUrl, isLargeRow }) {
             {movies.map((movie) => (
                     <img 
                     key={movie.id}
+                    onClick={() => handleClick(movie)}
                     className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                     src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path }`} 
                     alt={movie.name}/>
             ))}
         </div>
-        {/* container */}
-
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
-    )
+    );
 }
 export default Row
